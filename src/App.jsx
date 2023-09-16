56aaeeb42a49f80e13541155db61a2f55aae4c83
Abdel-Khafid kolawole Fifa SALAOU <119133987+adeola003@users.noreply.github.com>
2023-09-16 19:10:49 +0100

    install react router and create other components and added conditional rendering for navbar and sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
+import Navbar from './components/navbar'
 import viteLogo from '/vite.svg'
 import './App.css'
 
@@ -9,6 +11,7 @@ function App() {
   return (
     <>
       <div>
+        <Navbar />
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { NavLink } from 'react-router-dom';
+import classNames from 'classnames';
 
-const navbar = () => {
+const Navbar = () => {
+  const [isLargeScreen, setIsLargeScreen] = useState(window.innerWidth >= 768);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsLargeScreen(window.innerWidth >= 768);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  const navbarClasses = classNames('navbar', {
+    'navbar-hidden': isLargeScreen,
+  });
+
+  const sidebarClasses = classNames('sidebar', {
+    'sidebar-hidden': !isLargeScreen,
+  });
+
   return (
-    <div>navbar</div>
-  )
-}
+    <div>
+      <div className={sidebarClasses}>sidebar</div>
+      <nav className={navbarClasses}>navbar</nav>
+    </div>
+  );
+};
 
-export default navbar
+export default Navbar;
