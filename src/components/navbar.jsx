c808f1e766d7530497f4ba83547a336d0c6b2e87
Abdel-Khafid kolawole Fifa SALAOU <119133987+adeola003@users.noreply.github.com>
2023-09-16 22:22:21 +0100

    add basic styles to navbar and sidebar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
 import classNames from 'classnames';
+import { FaHome, FaUser, FaBriefcase, FaEnvelope, FaTrash } from 'react-icons/fa';
 import "../styles/navbar.css"
 
 const Navbar = () => {
@@ -26,6 +27,10 @@ const Navbar = () => {
     'sidebar-hidden': !isLargeScreen,
   });
 
+  const iconStyle = {
+    marginRight: '10px',
+  };
+
   return (
     <div>
       <div className={sidebarClasses}>
@@ -34,30 +39,35 @@ const Navbar = () => {
             <li>
             </li>
             <li>
-                <NavLink exact to="/">
-                    Home
-                </NavLink>
-            </li>
-            <li>
-                <NavLink exact to="/about">
-                    About
-                </NavLink>
-            </li>
-            <li>
-                <NavLink exact to="/portfolio">
-                    Portfolio
-                </NavLink>
-            </li>
-            <li>
-                <NavLink exact to="/contact">
-                    Contact
-                </NavLink>
-            </li>
-            <li>
-                <NavLink exact to="/trash">
-                    Trash
-                </NavLink>
-            </li>
+            <NavLink exact to="/" activeClassName='active-link'>
+              <FaHome style={iconStyle} /> {/* Icon */}
+              <span className="link-text">Home</span>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink exact to="/about" activeClassName='active-link'>
+              <FaUser style={iconStyle} /> {/* Icon */}
+              <span className="link-text">About</span>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink exact to="/portfolio" activeClassName='active-link'>
+              <FaBriefcase style={iconStyle} /> {/* Icon */}
+              <span className="link-text">Portfolio</span>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink exact to="/contact" activeClassName='active-link'>
+              <FaEnvelope style={iconStyle} /> {/* Icon */}
+              <span className="link-text">Contact</span>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink exact to="/trash" activeClassName='active-link'>
+              <FaTrash style={iconStyle} /> {/* Icon */}
+              <span className="link-text">Trash</span>
+            </NavLink>
+          </li>
         </ul></div>
       <nav className={navbarClasses}>
         <div className='nav-logo logo'>Adeola</div>
