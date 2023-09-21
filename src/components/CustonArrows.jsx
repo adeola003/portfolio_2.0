04774752ed2ae48c7748c9bf2a6ed137b7ac19f5
Abdel-Khafid kolawole Fifa SALAOU <119133987+adeola003@users.noreply.github.com>
2023-09-21 07:34:28 +0100

    create caroussel for potfolio section

--- src/components/CustonArrows.jsx
+++ src/components/CustonArrows.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { FaArrowLeft, FaArrowRight } from 'react-icons/fa';
+import "../styles/arrows.css";
 
 const CustomPrevArrow = (props) => (
   <div className="custom-arrow custom-prev-arrow" onClick={props.onClick}>
--- src/components/pages/portfolio.jsx
+++ src/components/pages/portfolio.jsx
@@ -2,27 +2,26 @@ import React from 'react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-import { FaArrowLeft, FaArrowRight } from 'react-icons/fa';
+import { CustomPrevArrow, CustomNextArrow } from '../CustonArrows';
+
 
 const Portfolio = () => {
   const settings = {
-    // Slick settings go here
-    // For example:
     dots: true,
     infinite: true,
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
-  //   prevArrow: <CustomPrevArrow />,
-  // nextArrow: <CustomNextArrow />,
+    prevArrow: <CustomPrevArrow />,
+  nextArrow: <CustomNextArrow />,
 
   };
 
   return (
     <div>
-      <h2>My Portfolio</h2>
+      <h2>Discover what I do</h2>
       <Slider {...settings}>
-        <div>
+        <div className='project-card'>
           <h3>Slide 1</h3>
         </div>
         <div>
@@ -31,7 +30,7 @@ const Portfolio = () => {
         <div>
           <h3>Slide 3</h3>
         </div>
-        {/* Add more slides as needed */}
+
       </Slider>
     </div>
   );
