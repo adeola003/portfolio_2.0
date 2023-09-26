b2bc10defb5a7adb52c960ed45a7dc55e0c996f5
Abdel-Khafid kolawole Fifa SALAOU <119133987+adeola003@users.noreply.github.com>
2023-09-26 18:40:42 +0100

    add github links

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -20,7 +20,10 @@ const ProjectCard = ({ project }) => {
         <h3>{project.name}</h3>
         <p>{project.description}</p>
         <p>Technologies: {project.techStack.join(', ')}</p>
-        <button className='details-btn' onClick={openModal}>Details</button>
+        <div className='project-buttons'>
+          <button className='details-btn' onClick={openModal}>Details</button>
+          <button className='details-btn'><a href={project.github} target='blank'>See source</a></button>
+        </div>
       </div>
       {isModalOpen && (
         <Modal onClose={closeModal}>
